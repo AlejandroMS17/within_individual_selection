0c90ea5c3def96d86fbda197c5bb06ed4aa16988
roblanf <rob.lanfear@gmail.com>
2018-05-24 11:53:37 +1000

    remove genomescope for now

--- mash.sh
+++ mash.sh
@@ -1,4 +1,4 @@
-outputf="/data/within_individual_selection/mash/"
+outputf="/data/within_individual_selection/"
 
 mkdir $outputf
 
@@ -13,19 +13,14 @@ kmer_max=5000 # ignore kmers represented more the kmer_max times (avoids issues 
 cd $outputf
 
 for tree in RB5 RB7 RB98; do
-
 	for branch in A B C D E F G H; do
-
 		# this uses info in a tsv file to find all fastq files from one branch. Should be 12 files in most of our datasets
 		# 12 files are forward and reverse reads, for each of 3 replicate samples, sequenced independently on two lanes (2*3*2=12)
 		fastqs=$(awk -v tree=$tree -v branch=$branch -F"\t" '$1 == tree && $2 == branch { print "/data/raw_data/"tree"/"$3"/"$4 }' sequencing_file_details.tsv)
-
 		prefix=$tree"_"$branch
-
 		# cat the fastqs and pipe to mash (stdin input is a - in -r)
 		# NB - cat seems so slow thta mash only runs on one CPU
 		#cat $fastqs | mash sketch -k 21 -r - -p $threads -m $m -k $kmer_size -s $s -o $prefix
-
 		# cat first, mash second
 		cat $fastqs > $prefix".fastq.gz"
 	done
@@ -33,17 +28,17 @@ done
 
 # first we do genomescoepe to help choose parameters for the mash analysis and QC the data
 
-for readf in $(find *.fastq.gz); do
-
-	# count kmers in jellyfish, then make a histogram. NB: use zcat because jellyfish needs raw fastq, not .gz
-	zcat $readf | jellyfish count -C -m $kmer_size -s $sg -t $threads -o $outputf$readf'.jf' /dev/fd/0
-	jellyfish histo -t $threads $outputf$readf'.jf' > $readf'.histo'
-
-	# run genomescope and put the output in a new directory
-	id=`echo "$readf" | cut -d'.' -f1`
-	genomescope.R $readf'.histo' $kmer_size $read_length $outputf$id $kmer_max verbose
-
-done
+#for readf in $(find *.fastq.gz); do
+#
+#	# count kmers in jellyfish, then make a histogram. NB: use zcat because jellyfish needs raw fastq, not .gz
+#	zcat $readf | jellyfish count -C -m $kmer_size -s $sg -t $threads -o $outputf$readf'.jf' /dev/fd/0
+#	jellyfish histo -t $threads $outputf$readf'.jf' > $readf'.histo'
+#
+#	# run genomescope and put the output in a new directory
+#	id=`echo "$readf" | cut -d'.' -f1`
+#	genomescope.R $readf'.histo' $kmer_size $read_length $outputf$id $kmer_max verbose
+#
+#done
 
 
 # now we mash
